493e1358a5f7dec9048b0e12cbf4d57328581acf
Prashanth Rajagopal <prashanth@browserstack.com>
2015-02-23 00:50:01 +0530

    Initial Commit

---
+++ ios_hello_world/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_hello_world
+//
+//  Created by Prashanth on 23/02/15.
+//  Copyright (c) 2015 nwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_hello_worldTests/ios_hello_worldTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_hello_worldTests.m
+//  ios_hello_worldTests
+//
+//  Created by Prashanth on 23/02/15.
+//  Copyright (c) 2015 nwo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_hello_worldTests : XCTestCase
+
+@end
+
+@implementation ios_hello_worldTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
